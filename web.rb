0f60a7970402c2d4c5041b166cb113d8cb77b95c
Luiz Picolo <luizpicolo@gmail.com>
2018-09-26 12:50:17 -0400

    Adicionado direcionamento para siai.nv

--- web.rb
+++ web.rb
@@ -4,6 +4,10 @@ get '/' do
    erb :home
 end
 
-get '/siai' do
+get '/siai.na' do
   redirect 'http://sistemas.na.ifms.edu.br:2697/'
+end
+
+get '/siai.nv' do
+  redirect 'http://sistemas.na.ifms.edu.br:20435/'
 end
