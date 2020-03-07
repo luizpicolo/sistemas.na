b4a2c5f4b6377de6d7471ce49ed04a32d8d112f2
Luiz Picolo ツ <luizpicolo@gmail.com>
2020-03-07 09:43:41 -0400

    Corrido caminhos de acesso

--- web.rb
+++ web.rb
@@ -2,14 +2,14 @@ require 'sinatra'
 
 base_url = 'http://sistemas.na.ifms.edu.br'
 paths = [
-  ['sigoe.na', 'Nova Andradina', '2697'],
-  ['sigoe.nv', 'Naviraí',        '2696'],
-  ['sigoe.pp', 'Ponta Porã',     '2693'],
-  ['sigoe.tl', 'Três Lagoas',    '2692'],
-  ['sigoe.aq', 'Aquidauana',     '2691'],
-  ['sigoe.cb', 'Corumbá',        '2690'],
-  ['sigoe.dr', 'Dourados',       '2689'],
-  ['sigoe.cx', 'Coxim',          '2688'],
+  ['/sigoe.na', 'Nova Andradina', '2697'],
+  ['/sigoe.nv', 'Naviraí',        '2696'],
+  ['/sigoe.pp', 'Ponta Porã',     '2693'],
+  ['/sigoe.tl', 'Três Lagoas',    '2692'],
+  ['/sigoe.aq', 'Aquidauana',     '2691'],
+  ['/sigoe.cb', 'Corumbá',        '2690'],
+  ['/sigoe.dr', 'Dourados',       '2689'],
+  ['/sigoe.cx', 'Coxim',          '2688'],
 ]
 
 get '/' do
@@ -19,7 +19,7 @@ get '/' do
 end
 
 paths.each do |path|
-  get path do
+  get path.first do
     redirect "#{base_url}:#{path.last}/"
   end
 end
